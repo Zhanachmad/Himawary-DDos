81df060914631e87fc356be1d5d62277473808bd
Kafulkaff99 <kafulkaff02@gmail.com>
2024-10-20 10:35:12 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+
+print("""[0] pip\n[1] pip3\nWhich one do you use?""")
+
+c = input(">>>: ")
+if c == "0":
+    os.system("pkg update && pkg upgrade")
+    os.system("pkg install python")
+    os.system("pkg install python2")
+    os.system("pkg install python3")
+    os.system("pip install colorama")
+    os.system("pip install requests")
+
+elif c == "1":
+
+os.system("pkg update && pkg upgrade")
+    os.system("pkg install python")
+    os.system("pkg install python2")
+    os.system("pkg install python3")
+    os.system("pip install colorama")
+    os.system("pip install requests")
+print("Done.")
