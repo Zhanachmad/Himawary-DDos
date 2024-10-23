a30bfe0673251909c54a1fb4f66af48fb39cc7e2
Kafulkaff99 <kafulkaff02@gmail.com>
2024-10-23 12:17:55 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ print("""[0] pip\n[1] pip3\nWhich one do you use?""")
 
 c = input(">>>: ")
 if c == "0":
-    os.system("pkg install python")
-    os.system("pkg install python2")
-    os.system("pkg install python3")
-    os.system("pip install colorama")
-    os.system("pip install requests")
+    os.system("apt install python")
+    os.system("apt install python2")
+    os.system("apt install python3")
+    os.system("apt install colorama")
+    os.system("apt install requests")
 
 elif c == "1":
 
-    os.system("pkg install python")
-    os.system("pkg install python2")
-    os.system("pkg install python3")
-    os.system("pip install colorama")
-    os.system("pip install requests")
+    os.system("apt install python")
+    os.system("apt install python2")
+    os.system("apt install python3")
+    os.system("apt install colorama")
+    os.system("apt install requests")
 print("Done.")
